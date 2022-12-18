cb51bc80fbaa2330cb7a59ff18c2f1881b46c6a9
Jody LeCompte <jody@jodylecompte.com>
2022-12-18 12:59:16 -0600

    Hide projects link temporarily

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,22 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react';
 
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
+import { Footer, Header } from '@/components/global';
 
-import '@/styles/tailwind.css'
-import 'focus-visible'
+import '@/styles/tailwind.css';
+import 'focus-visible';
 
 function usePrevious(value) {
-  let ref = useRef()
+  let ref = useRef();
 
   useEffect(() => {
-    ref.current = value
-  }, [value])
+    ref.current = value;
+  }, [value]);
 
-  return ref.current
+  return ref.current;
 }
 
 export default function App({ Component, pageProps, router }) {
-  let previousPathname = usePrevious(router.pathname)
+  let previousPathname = usePrevious(router.pathname);
 
   return (
     <>
@@ -34,5 +33,5 @@ export default function App({ Component, pageProps, router }) {
         <Footer />
       </div>
     </>
-  )
+  );
 }
