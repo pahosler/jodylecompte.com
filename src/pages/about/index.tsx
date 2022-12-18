dfcf7fbe88f46702a7d5b8f304abd9a4843f26ea
Jody LeCompte <jody@jodylecompte.com>
2022-12-18 17:59:58 -0600

    About us page v1

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -61,62 +61,65 @@ export default function About() {
       </Head>
       <Container className="mt-16 sm:mt-32">
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-          <div className="lg:pl-20">
-            <div className="max-w-xs px-2.5 lg:max-w-none">
-              <Image
-                src={portraitImage}
-                alt=""
-                sizes="(min-width: 1024px) 32rem, 20rem"
-                className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-              />
-            </div>
-          </div>
+          <div className="lg:pl-20"></div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Jody LeCompte. I live in New York City, where I design the
-              future.
+              I’m Jody LeCompte. I live in New Orleans, where I convert pizza
+              and caffiene into software.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I’ve been obsessed with creation and with computers for as long
+                as I can remember. My dad was a "webmaster" (who remembers that
+                term?) in the late 90's and after my curiosity got the best of
+                me, he began teaching me HTML.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                Fast forward some years later, I watched the internet become a
+                massive thing so much more than I ever could have dreamed of it
+                being as a kid who saw the internet as Flash games and
+                discussion forums. I went from "study your math because you
+                won't have a calculator every day" to carrying a super computer
+                with the entirety of human knowledge in my back pocket. Funnily
+                enough, it's also a calculator.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                After high school, I spent many years honing my skills and
+                working my way up from a support engineer working in a call
+                center, before starting my journey as a professional developer
+                working with popular PHP based stacks like CodeIgniter and
+                Laravel.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                Today, I have the best job in the world working as a full-stack
+                developer specializing in TypeScript, React, and accessible
+                design. When I'm not converting problems into solutions for my
+                clients, I am working on sharing my experience and knowledge to
+                help empower other developers to reach their full potential
+                while I work to realize my own.
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
-              <SocialLink href="#" icon={TwitterIcon}>
+              <SocialLink
+                href="https://twitter.com/jody_lecompte"
+                icon={TwitterIcon}
+              >
                 Follow on Twitter
               </SocialLink>
-              <SocialLink href="#" icon={InstagramIcon} className="mt-4">
-                Follow on Instagram
-              </SocialLink>
-              <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+              <SocialLink
+                href="https://github.com/jodylecompte"
+                icon={GitHubIcon}
+                className="mt-4"
+              >
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+              <SocialLink
+                href="https://linkedin.com/in/jodylecompte"
+                icon={LinkedInIcon}
+                className="mt-4"
+              >
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
@@ -124,7 +127,7 @@ export default function About() {
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                spencer@planetaria.tech
+                jody@jodylecompte.com
               </SocialLink>
             </ul>
           </div>
