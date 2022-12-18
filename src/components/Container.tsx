72c86e76f7954cfb1df7f37c1e011234d805d60c
Jody LeCompte <jody@jodylecompte.com>
2022-12-18 08:57:34 -0600

    Change all files to TypeScript extension

---
+++ src/components/Container.tsx
@@ -0,0 +1,32 @@
+import { forwardRef } from 'react';
+import clsx from 'clsx';
+
+const OuterContainer = ({ className, children, ...props }) => {
+  return (
+    <div className={clsx('sm:px-8', className)} {...props}>
+      <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
+    </div>
+  );
+};
+
+const InnerContainer = ({ className, children, ...props }) => {
+  return (
+    <div
+      className={clsx('relative px-4 sm:px-8 lg:px-12', className)}
+      {...props}
+    >
+      <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+    </div>
+  );
+};
+
+export const Container = ({ children, ...props }) => {
+  return (
+    <OuterContainer {...props}>
+      <InnerContainer>{children}</InnerContainer>
+    </OuterContainer>
+  );
+};
+
+Container.Outer = OuterContainer;
+Container.Inner = InnerContainer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -101,14 +101,9 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Father, Fisherman and Full-Stack Developer
           </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
-          </p>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400"></p>
           <div className="mt-6 flex gap-6">
             <SocialLink
               href="https://twitter.com"
