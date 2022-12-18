d1305b28f409d3dfe8f9b7caa1e0a6ca9dfa8515
Jody LeCompte <jody@jodylecompte.com>
2022-12-18 12:32:18 -0600

    Make social links open in new tab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,7 +69,7 @@ function Article({ article }) {
 
 function SocialLink({ icon: Icon, href, ...props }) {
   return (
-    <Link className="group -m-1 p-1" href={href} {...props}>
+    <Link className="group -m-1 p-1" href={href} {...props} target="_blank">
       <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
     </Link>
   );
