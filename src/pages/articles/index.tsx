78b08bed490b6a37f0f16fa9e9e12d3405acf0db
Jody LeCompte <jody@jodylecompte.com>
2022-12-18 16:09:52 -0600

    Articles page v1

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -41,12 +41,12 @@ export default function ArticlesIndex({ articles }) {
         <title>Articles - Jody LeCompte</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="A collection of tutorial, articles, and thoughts about software development, accessibility, and being a career professional developer."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Articles"
+        intro="A collection of tutorials, articles, and thoughts about software development, accessibility, and being a career professional developer"
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
