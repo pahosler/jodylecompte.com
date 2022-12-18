3fa26262898ce7946ef7222fa0ee15e20e1639e5
Jody LeCompte <jody@jodylecompte.com>
2022-12-18 11:26:02 -0600

    Rough prototype of article loading -- will need to be expanded for internationalization and pagination eventually

--- src/lib/getAllArticles.ts
+++ src/lib/getAllArticles.ts
@@ -1,23 +1,26 @@
-import glob from 'fast-glob'
-import * as path from 'path'
+import glob from 'fast-glob';
+import * as path from 'path';
 
+// TODO: Currently all articles are English, but this code will need to be modified
+// eventually to support a top level directory for each language that articles may
+// exist in
 async function importArticle(articleFilename) {
   let { meta, default: component } = await import(
-    `../pages/articles/${articleFilename}`
-  )
+    `../pages/articles/en/${articleFilename}`
+  );
   return {
     slug: articleFilename.replace(/(\/index)?\.mdx$/, ''),
     ...meta,
     component,
-  }
+  };
 }
 
 export async function getAllArticles() {
   let articleFilenames = await glob(['*.mdx', '*/index.mdx'], {
-    cwd: path.join(process.cwd(), 'src/pages/articles'),
-  })
+    cwd: path.join(process.cwd(), 'src/pages/articles/en/'),
+  });
 
-  let articles = await Promise.all(articleFilenames.map(importArticle))
+  let articles = await Promise.all(articleFilenames.map(importArticle));
 
-  return articles.sort((a, z) => new Date(z.date) - new Date(a.date))
+  return articles.sort((a, z) => new Date(z.date) - new Date(a.date));
 }
