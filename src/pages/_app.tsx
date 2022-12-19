00923ad67ffe96aa8c284838a0ee873acf281a7f
Jody LeCompte <jody@jodylecompte.com>
2022-12-19 08:25:26 -0600

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 import { useEffect, useRef } from 'react';
 
 import { Footer, Header } from '@/components/global';
@@ -20,6 +21,7 @@ export default function App({ Component, pageProps, router }) {
 
   return (
     <>
+      <GoogleAnalytics trackPageViews />
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
