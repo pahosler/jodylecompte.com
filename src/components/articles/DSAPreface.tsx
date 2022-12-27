748525ee84ae1a911227e71e23f7a9bd51a69946
Jody LeCompte <jody@jodylecompte.com>
2022-12-27 11:48:37 -0600

    WIP on sorting algorithms series

---
+++ src/components/articles/DSAPreface.tsx
@@ -0,0 +1,25 @@
+export function DSAPreface() {
+  return (
+    <>
+      <h2>Series Preface</h2>
+      <p>
+        In this series we will be breaking down how nine of the most common
+        sorting algorithms work and implementing them from scratch in
+        JavaScript. Algorithms can often stress people out, but don't worry! If
+        you have a basic understanding of JavaScript, you'll be good to go!
+      </p>
+      <p>
+        Please keep in mind, the code in this series is for demonstrative
+        purposes only and should not be used in production. In the overwhelming
+        majority of cases, you will want to opt for one of the sorting
+        algorithms built-in to your language of choice rather than rolling your
+        own.
+      </p>
+      <p>
+        <a href="/articles/en/demystifying-sorting-algorithms/">
+          List of all articles in this series
+        </a>
+      </p>
+    </>
+  );
+}
